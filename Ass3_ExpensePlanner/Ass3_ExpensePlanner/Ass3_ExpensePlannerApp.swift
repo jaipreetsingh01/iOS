879beac01a5e94fc5fa818ba88d97064e1cd72d7
Jaipreet Singh <40367262+jaipreetsingh01@users.noreply.github.com>
2024-05-10 12:52:02 +1000

    Setup - Getting structure from Kendrick & Lance

---
+++ Ass3_ExpensePlanner/Ass3_ExpensePlanner/Ass3_ExpensePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ass3_ExpensePlannerApp.swift
+//  Ass3_ExpensePlanner
+//
+//  Created by Jaipreet  on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Ass3_ExpensePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ass3_ExpensePlanner/Ass3_ExpensePlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ass3_ExpensePlanner
+//
+//  Created by Jaipreet  on 10/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
